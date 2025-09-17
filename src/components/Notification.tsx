e7afaedf26c9d109580408faa1b65c6a8489d7dc
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-09-18 00:08:32 +0600

    some page and components add

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notification = () => {
+    return (
+        <div className='h-12 bg-red-500 text-sm text-white px-4 flex items-center justify-center text-center md:text-base cursor-pointer'>
+            
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        </div>
+    );
+};
+
+export default Notification;
